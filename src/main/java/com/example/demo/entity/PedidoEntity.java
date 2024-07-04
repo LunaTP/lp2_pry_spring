57d6209eaf04dfc247a11581e3f2e3e64a798b0c
Diego Alejandro Luna Tarapa <diegolt2014@gmail.com>
2024-07-04 12:46:59 -0500

    Tercer commit, entidades corregidas

--- src/main/java/com/example/demo/entity/PedidoEntity.java
+++ src/main/java/com/example/demo/entity/PedidoEntity.java
@@ -1,13 +1,16 @@
 package com.example.demo.entity;
 
 import java.time.LocalDate;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -34,4 +37,7 @@ public class PedidoEntity {
 	@ManyToOne
 	@JoinColumn(name = "usuario_id", nullable = false)
 	private UsuarioEntity usuarioEntity;
+	
+	@OneToMany(mappedBy = "pedidoEntity",cascade = CascadeType.ALL)
+	private List<DetallePedidoEntity> detallePedido;
 }
